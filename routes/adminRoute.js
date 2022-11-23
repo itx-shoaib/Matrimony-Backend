a0d177f64ef9cd2d5e145273c5c2c36900b0b2e1
itx-shoaib <shoaibjamil43@gmail.com>
2022-11-23 13:36:03 +0500

    making adminRoute route

---
+++ routes/adminRoute.js
@@ -0,0 +1,11 @@
+const express = require('express'); //import express
+
+// 1.
+const router = express.Router();
+// 2.
+const AdminController = require('../controllers/adminRoute');
+// 3.
+router.get('/getallusers', AdminController.AllUser);
+
+// 4. 
+module.exports = router; // export to use in server.js
