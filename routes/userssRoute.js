3b31dee01c0cd80add40c4b5e0e2dcaed8423996
awais <awais.shahbaz@invozone.com>
2022-12-30 02:09:18 +0500

    chat added

--- routes/userssRoute.js
+++ routes/userssRoute.js
@@ -4,6 +4,8 @@ const express = require('express'); //import express
 const router = express.Router();
 // 2.
 const UserController = require('../controllers/userssRoute');
+const chat = require('../controllers/chatRoute');
+
 // 3.
 router.post('/Onlineuser', UserController.OnlineUser);
 // 4. 
@@ -15,4 +17,8 @@ router.post('/viewRequest', UserController.viewRequest);
 router.post('/sentRequest', UserController.sentRequest);
 router.post('/nearBy', UserController.nearBy);
 router.post('/viewAllRequest', UserController.viewAllRequest);
+router.post('/getAllChat', chat.getAllChat);
+router.post('/getChatGroup', chat.getChatGroup);
+router.post('/postchat', chat.postchat);
+
 module.exports = router; // export to use in server.js
